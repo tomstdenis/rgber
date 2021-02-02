5ef233c5ce12199c5c0e1aff81c2590006b1464f
tomstdenis <tstdenis82@gmail.com>
2021-02-02 14:12:31 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
-namespace rgber {
-    export function plot (x: number, y: number, r: number, g: number, b: number) {
-    let i = 0
+function plot (x: number, y: number, r: number, g: number, b: number) {
     i = x * 8
     if (x / 2 == 0) {
-        i = x + y
+        i = i + y
     } else {
-        i = x + (7 - y)
+        i = i + (7 - y)
     }
     strip.setPixelColor(i, neopixel.rgb(r, g, b))
 }
-let strip: neopixel.Strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
-}
+let i = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
