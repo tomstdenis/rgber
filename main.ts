cdd83966da0ce73640247deb1d13710318c4be11
tomstdenis <tstdenis82@gmail.com>
2021-02-02 13:57:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+function plot (x: number, y: number, r: number, g: number, b: number) {
+    i = x * 8
+    if (x / 2 == 0) {
+        i = x + y
+    } else {
+        i = x + (7 - y)
+    }
+    strip.setPixelColor(i, neopixel.rgb(r, g, b))
+}
+let i = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
